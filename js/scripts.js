5a8c50866c3f937d1c71121de74b6af1394055d7
Brad Pizzimenti and Jessica Fix <student@epicodus.com>
2016-01-20 11:20:14 -0800

    refactor first to include vowels array in a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 var pigLatin = function(englishPhrase) {
-  if (englishPhrase[0] === 'a' || englishPhrase[0] === 'e' || englishPhrase[0] === 'i' || englishPhrase[0] === 'o' || englishPhrase[0] === 'u') {
+  var vowels = ['a','e','i','o','u']
+
+
+
+  for (var index = 0; index < 5; index +=1) {
+
+    if (englishPhrase[0] === vowels[index]) {
     return englishPhrase + "ay";
-  } else if (englishPhrase[1] === 'a' || englishPhrase[1] === 'e' || englishPhrase[1] === 'i' || englishPhrase[1] === 'o' || englishPhrase[1] === 'u') {
-     return englishPhrase.slice(1) + englishPhrase[0] + "ay";
   }
-  return englishPhrase.slice(2) + englishPhrase[0] + englishPhrase[1] + "ay";
 }
+  //
+  // } else if (englishPhrase[1] === 'a' || englishPhrase[1] === 'e' || englishPhrase[1] === 'i' || englishPhrase[1] === 'o' || englishPhrase[1] === 'u') {
+  //    return englishPhrase.slice(1) + englishPhrase[0] + "ay";
+
+//   if (englishPhrase[0] === 'a' || englishPhrase[0] === 'e' || englishPhrase[0] === 'i' || englishPhrase[0] === 'o' || englishPhrase[0] === 'u') {
+//     return englishPhrase + "ay";
+//   } else if (englishPhrase[1] === 'a' || englishPhrase[1] === 'e' || englishPhrase[1] === 'i' || englishPhrase[1] === 'o' || englishPhrase[1] === 'u') {
+//      return englishPhrase.slice(1) + englishPhrase[0] + "ay";
+//   }
+//   return englishPhrase.slice(2) + englishPhrase[0] + englishPhrase[1] + "ay";
+};
