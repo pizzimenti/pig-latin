2100633781bb3bfa398c74bcf10db26694f3ebd7
Brad Pizzimenti and Jessica Fix <jcfix.pdx@gmail.com>
2016-01-20 08:43:00 -0800

    initial commit of bdd spec files

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var pigLatin = function(apple) {
+  return false;
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('pigLatin', function() {
+  it("it adds '-ay' to the end of all words", function() {
+    expect(leapYear(apple)).to.equal(apple-ay);
+  });
+});
