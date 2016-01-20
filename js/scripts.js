a4d4df9e3d41729ba4bd7772f3b1780175e47dfe
Brad Pizzimenti and Jessica Fix <brad@gennakersystems.com>
2016-01-20 10:38:49 -0800

    add third spec and working logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 var pigLatin = function(englishPhrase) {
   if (englishPhrase[0] === 'a' || englishPhrase[0] === 'e' || englishPhrase[0] === 'i' || englishPhrase[0] === 'o' || englishPhrase[0] === 'u') {
     return englishPhrase + "ay";
-  } else {
+  } else if (englishPhrase[1] === 'a' || englishPhrase[1] === 'e' || englishPhrase[1] === 'i' || englishPhrase[1] === 'o' || englishPhrase[1] === 'u') {
      return englishPhrase.slice(1) + englishPhrase[0] + "ay";
   }
+  return englishPhrase.slice(2) + englishPhrase[0] + englishPhrase[1] + "ay";
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,12 @@
 describe('pigLatin', function() {
   it("it adds 'ay' to the end of all words", function() {
     expect(pigLatin("apple")).to.equal("appleay");
-  });
+  })
 
-  it("relocates consonants from front to end of word", function() {
+  it("relocates first consonant from front to end of word", function() {
     expect(pigLatin("lunch")).to.equal("unchlay");
-  });
-
+  })
+  it("relocates first and second consonant from the front to the back", function() {
+    expect(pigLatin("change")).to.equal("angechay");
+  })
 });
